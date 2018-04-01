8f361c2959b98fd293752e1780bd3e25759a900b
Rob Laptop <r.t.waller@gmail.com>
2018-04-01 12:20:35 -0500

    Update route for Django 2

--- musicBlog/articles/urls.py
+++ musicBlog/articles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.article_list)
+    path('', views.article_list),
+    path(r'^(?P<slug>[\w-]+)/$', views.article_details)
 ]
